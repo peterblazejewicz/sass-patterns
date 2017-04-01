4a4d7cd6bd49af0f7cc3e8c0a27d46de81b45acc
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-04-01 09:57:59 +0200

    Setup tooling for SCSS to CSS development
    - browser-sync
    - node-sass
    - NPM tasks
    - concurent execution with *watch* options

---
+++ bs-config.js
@@ -0,0 +1,71 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "ui": false,
+    "files": [
+        'index.html',
+        './css/*.css'
+    ],
+    "watchEvents": [
+        "change"
+    ],
+    "watchOptions": {
+        "ignoreInitial": true
+    },
+    "server": true,
+    "port": 3000,
+    "middleware": false,
+    "serveStatic": [],
+    "logLevel": "info",
+    "logPrefix": "sass",
+    "logConnections": false,
+    "logFileChanges": true,
+    "logSnippet": true,
+    "rewriteRules": [],
+    "open": "local",
+    "browser": "default",
+    "cors": false,
+    "xip": false,
+    "hostnameSuffix": false,
+    "reloadOnRestart": false,
+    "notify": true,
+    "scrollProportionally": true,
+    "scrollThrottle": 0,
+    "scrollRestoreTechnique": "window.name",
+    "scrollElements": [],
+    "scrollElementMapping": [],
+    "reloadDelay": 0,
+    "reloadDebounce": 0,
+    "reloadThrottle": 0,
+    "plugins": [],
+    "injectChanges": true,
+    "startPath": null,
+    "minify": true,
+    "host": null,
+    "localOnly": false,
+    "codeSync": true,
+    "timestamps": true,
+    "tagNames": {
+        "less": "link",
+        "scss": "link",
+        "css": "link",
+        "jpg": "img",
+        "jpeg": "img",
+        "png": "img",
+        "svg": "img",
+        "gif": "img",
+        "js": "script"
+    }
+};
